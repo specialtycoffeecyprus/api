0951e7a2e827fefaed819faee58ff247aab294bc
Vladimir Martsul <vladimir.martsul@alphawave.ru>
2022-07-06 16:44:54 +0300

    Add CORS restrictions
    Signed-off-by: Vladimir Martsul <vladimir.martsul@alphawave.ru>

--- config/cors.php
+++ config/cors.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'paths' => ['*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_methods' => ['GET'],
+    'allowed_origins' => [env('CORS_ORIGIN', '*')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
