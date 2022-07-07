c025fd15df3f2148484e83d4e55c2073ff46f64b
Vladimir Martsul <vladimir.martsul@alphawave.ru>
2022-07-07 11:46:54 +0300

    Add Sentry
    Signed-off-by: Vladimir Martsul <vladimir.martsul@alphawave.ru>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
     public function register(): void
     {
+        $this->reportable(static function (Throwable $e): void {
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
+        });
     }
 }
