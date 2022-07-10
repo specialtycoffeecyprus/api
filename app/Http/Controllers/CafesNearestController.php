cca2f7aa93e6c1f813ba9b59479a1e153b4073be
Vladimir Martsul <vladimir.martsul@alphawave.ru>
2022-07-11 02:07:19 +0300

    Add /cafes/nearest endpoint
    Signed-off-by: Vladimir Martsul <vladimir.martsul@alphawave.ru>

--- app/Http/Controllers/CafesNearestController.php
+++ app/Http/Controllers/CafesNearestController.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CafesNearestRequest;
-use App\Http\Resources\CafeCollection;
+use App\Http\Resources\CafeResource;
 use App\Models\Cafe;
+
+use function print_r;
 
 class CafesNearestController extends Controller
 {
-    public function __invoke(CafesNearestRequest $request): CafeCollection
+    public function __invoke(CafesNearestRequest $request): CafeResource
     {
         $validated = $request->safe(['latitude', 'longitude']);
         [$latitude, $longitude] = [$validated['latitude'], $validated['longitude']];
 
-        return new CafeCollection(Cafe::orderByRaw('(latitude - ?) * (latitude - ?) + (longitude - ?) * (longitude - ?)', [$latitude, $latitude, $longitude, $longitude])->take(1)->get());
+        return new CafeResource(Cafe::orderByRaw('(latitude - ?) * (latitude - ?) + (longitude - ?) * (longitude - ?)', [$latitude, $latitude, $longitude, $longitude])->first());
     }
 }
