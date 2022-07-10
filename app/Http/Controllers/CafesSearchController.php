791001cccfb44b02fa533393b5e29154fd4e7f15
Vladimir Martsul <vladimir.martsul@alphawave.ru>
2022-07-10 16:48:57 +0300

    Add search by name endpoint
    Signed-off-by: Vladimir Martsul <vladimir.martsul@alphawave.ru>

---
+++ app/Http/Controllers/CafesSearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CafeSearchRequest;
+use App\Http\Resources\CafeCollection;
+use App\Models\Cafe;
+
+class CafesSearchController extends Controller
+{
+    public function __invoke(CafeSearchRequest $request): CafeCollection
+    {
+        $q = $request->safe()['q'];
+
+        return new CafeCollection(Cafe::where('name', 'like', "%{$q}%")->get());
+    }
+}
---
+++ app/Http/Requests/CafeSearchRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CafeSearchRequest extends FormRequest
+{
+    /** @return array<string, string> */
+    public function rules(): array
+    {
+        return ['q' => ['required', 'string', 'max:255']];
+    }
+}
