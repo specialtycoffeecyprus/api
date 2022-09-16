bf7ffcf98152a61d933f83037ed752e55c973068
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-09-16 17:27:59 +0300

    Restore TrustProxies for the right detecting HTTPS
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,7 @@ namespace App\Http;
 use App\Http\Middleware\JsonResponse;
 use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use App\Http\Middleware\TrimStrings;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
@@ -20,6 +21,7 @@ final class Kernel extends HttpKernel
 {
     /** @var string[] */
     protected $middleware = [
+        TrustProxies::class,
         HandleCors::class,
         PreventRequestsDuringMaintenance::class,
         ValidatePostSize::class,
---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /** @inheritdoc */
+    protected $proxies = '*';
+}
