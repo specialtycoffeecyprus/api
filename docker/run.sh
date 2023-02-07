a3dbdcd2a913a2671a726c9c320d59ac50f8683f
Vladimir Martsul <vladimir.martsul@gmail.com>
2023-02-08 01:39:17 +0200

    Upgrade Docker configuration
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 touch database/database.sqlite \
-&& php artisan migrate --force --no-ansi --no-interaction \
+&& php artisan migrate --seed --force --no-ansi --no-interaction \
+&& php artisan optimize --no-ansi --no-interaction \
 && php artisan event:cache --no-ansi --no-interaction \
-&& php artisan route:cache --no-ansi --no-interaction \
 && php artisan view:cache --no-ansi --no-interaction \
 && chown -R www-data:www-data \
     /var/www/html/bootstrap/cache/ \
