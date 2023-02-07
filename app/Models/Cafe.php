3332d18a63d7b667d877f0f71a176c890f7a55aa
Vladimir Martsul <vladimir.martsul@gmail.com>
2023-02-07 18:28:06 +0200

    Drop SoftDeletes support
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- app/Models/Cafe.php
+++ app/Models/Cafe.php
@@ -6,19 +6,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
 final class Cafe extends Model
 {
     use HasFactory;
     use Searchable;
-    use SoftDeletes;
 
     protected $hidden = [
         'created_at',
         'updated_at',
-        'deleted_at',
     ];
 
 
--- database/migrations/2022_06_22_145012_create_cafes_table.php
+++ database/migrations/2022_06_22_145012_create_cafes_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
         Schema::create('cafes', static function (Blueprint $table): void {
             $table->id();
             $table->timestamps();
-            $table->softDeletes();
             $table->string('name')->index();
             $table->text('description')->index()->nullable();
             $table->string('code')->index();
