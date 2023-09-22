c0c0f6711d6cb0e62e0da165d2a75a44a27e61e3
Vladimir Martsul <vladimir.martsul@gmail.com>
2023-09-22 21:04:50 +0300

    Code style
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- rector.php
+++ rector.php
@@ -2,14 +2,19 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Assign\SplitListAssignToSeparateLineRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
 use Rector\Config\RectorConfig;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Php55\Rector\Class_\ClassConstantToSelfClassRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelLevelSetList;
 use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -18,22 +23,26 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
         SetList::NAMING,
         SetList::PRIVATIZATION,
         SetList::TYPE_DECLARATION,
         LevelSetList::UP_TO_PHP_82,
-        LaravelSetList::LARAVEL_100,
         LaravelSetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        LaravelLevelSetList::UP_TO_LARAVEL_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
     ]);
 
     $rectorConfig->skip([
+        ClassConstantToSelfClassRector::class,
         EncapsedStringsToSprintfRector::class,
         RenameParamToMatchTypeRector::class,
-        SplitListAssignToSeparateLineRector::class,
+        RenamePropertyToMatchTypeRector::class,
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
         WrapEncapsedVariableInCurlyBracesRector::class,
     ]);
 
